682ae1bc6cfcc4387ac0d76e11667d6ab0ddba6b
joseph skokan <joeskokan20@gmail.com>
2024-12-10 23:21:53 -0500

    more options

--- GameWIP/Space.s
+++ GameWIP/Space.s
@@ -3,6 +3,7 @@ welcome_msg:     .asciz "Welcome to the Space Game! Choose an option:\n1. Go to 
 go_space_msg:    .asciz "You have chosen to go to space! Blast off!\n"
 wait_msg:        .asciz "You have chosen to wait. Time is passing...\n"
 reload_msg:      .asciz "You choose to stay in town and reload your munitions...\n"
+merchant_msg:    .asciz "As you wait for your ships reload to complete, you begin seeing a few merchants heading your way\n"
 invalid_choice:  .asciz "Invalid choice. Please select 1, 2, or 3.\n"
 
 .section .bss
@@ -15,21 +16,21 @@ _start:
     # Print welcome message
     mov $1, %rax          # syscall: write
     mov $1, %rdi          # file descriptor: stdout
-    lea welcome_msg(%rip), %rsi # pointer to message
+    lea welcome_msg(%rip), %rsi # poinnter to message
     mov $78, %rdx         # message length
     syscall
 
     # Get user input (1 byte for choice)
     mov $0, %rax          # syscall: read
     mov $0, %rdi          # file descriptor: stdin
-    lea input_buf(%rip), %rsi # buffer to store input
+    lea input_buf(%rip), %rsi # bbuffer to store input
     mov $1, %rdx          # read 1 byte (one character)
     syscall
 
     # Process input
     movzbl input_buf(%rip), %eax # Load user input into rax
     cmp $49, %al          # Compare input with '1' (ASCII 49)
-    je go_to_space        # If '1', go to space
+    je go_to_space        # If '1, go to space
 
     cmp $50, %al          # Compare input with '2' (ASCII 50)
     je wait_for_time      # If '2', wait
@@ -57,7 +58,7 @@ wait_for_time:
     mov $1, %rax          # syscall: write
     mov $1, %rdi          # file descriptor: stdout
     lea wait_msg(%rip), %rsi # pointer to message
-    mov $41, %rdx         # message length
+    mov $41, %rdx         # message lenth
     syscall
     jmp _exit_game
 
@@ -66,6 +67,14 @@ reloading:
     mov $1, %rdi          # file descriptor: stdout
     lea reload_msg(%rip), %rsi  # pointer to reload message
     mov $55, %rdx         # reload message length
+    syscall
+    jmp merchant
+
+merchant:
+    mov $1, %rax
+    mov $1, %rdi
+    lea merchant_msg(%rip), %rsi
+    mov $80, %rdx
     syscall
     jmp _exit_game
 
