dbb698109137da0da284c38501eac0790cc8bc1d
joseph skokan <joeskokan20@gmail.com>
2025-03-29 15:47:10 -0400

    prompting for age

--- inputs/userAge.s
+++ inputs/userAge.s
@@ -3,12 +3,27 @@
 
 .section .data
     prompt: .asciz "Enter your age: "
+    message: .asciz "You are "
+    years: .asciz " years old"
 
 .section .text
 
 _start:
+    ; print the prompt for age
     mov rax, 1 ;sys call for stout
     mov rdi, 1 ;file descriptor for stdout
     lea rsi,[prompt] ; pointer to prompt string
     mov rdx, 20
     syscall
+    ; read the age entered
+    mov rax, 0
+    mov rdi, 0
+    lea rsi, [age]
+    mov rdx, 20
+
+    mov rax, 1
+    mov rdi, 1
+    lea rsi, [message]
+    mov rdx, 20
+    syscall
+
