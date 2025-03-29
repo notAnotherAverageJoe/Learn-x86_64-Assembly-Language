65663a7e843940131913cdf960ca3d01ca8a078e
joseph skokan <joeskokan20@gmail.com>
2025-03-29 15:49:22 -0400

    end prompt

--- inputs/userAge.s
+++ inputs/userAge.s
@@ -20,10 +20,23 @@ _start:
     mov rdi, 0
     lea rsi, [age]
     mov rdx, 20
-
+    syscall
+    ; print the message for age start
     mov rax, 1
     mov rdi, 1
     lea rsi, [message]
+    mov rdx, 20
+    syscall
+    ; print out age
+    mov rax, 1
+    mov rdi, 1
+    lea rsi, [age]
+    mov rdx, 20
+    syscall
+     ; print the message for age end
+    mov rax, 1
+    mov rdi, 1
+    lea rsi, [years]
     mov rdx, 20
     syscall
 
